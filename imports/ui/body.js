ae725311ddc5438bb8c9bbd2dcb651f6cad5c89b
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2017-04-04 15:39:17 -0500

    Complete tutorial, implement custom style

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,15 +1,45 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+
+    // If hide completed is checked, filter tasks
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find(
+        { checked: { $ne: true } },
+        {
+          sort: {
+            createdAt: -1,
+          },
+        },
+      );
+    }
+
+    // Otherwise, return all of the tasks
     return Tasks.find({}, {
       sort: { createdAt: -1 },
     });
+  },
+
+  incompleteCount() {
+    return Tasks.find({
+      checked: {
+        $ne: true,
+      },
+    }).count();
   },
 });
 
@@ -23,12 +53,13 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, templateInstance) {
+    templateInstance.state.set('hideCompleted', event.target.checked);
   },
 });
